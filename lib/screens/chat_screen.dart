e0be4e052eef1b4935c6d43a214ac7dbc194c68b
start <mrcs.stark@gmail.com>
2019-03-17 20:27:16 -0300

    Iniciando implementaçao do layout do chat

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -16,13 +16,12 @@ class _ChatScreenState extends State<ChatScreen> {
   WatsonAssistantCredential credential = WatsonAssistantCredential(
       username: Constants.USERNAME,
       password: Constants.PASSWORD,
-      workspaceId: Constants.WORKSPACEID 
-      );
+      workspaceId: Constants.WORKSPACEID);
 
   WatsonAssistantApiV1 watsonAssistant;
   WatsonAssistantResult watsonAssistantResponse;
   WatsonAssistantContext watsonAssistantContext =
-  WatsonAssistantContext(context: {});
+      WatsonAssistantContext(context: {});
 
   final myController = TextEditingController();
 
@@ -45,40 +44,64 @@ class _ChatScreenState extends State<ChatScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(
-              Icons.restore,
-            ),
-            onPressed: () {
-              watsonAssistantContext.resetContext();
-              setState(() {
-                _text = null;
-              });
-            },
-          )
-        ],
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+    return SafeArea(
+      bottom: false,
+      top: false,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+          centerTitle: true,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.restore,
+              ),
+              onPressed: () {
+                watsonAssistantContext.resetContext();
+                setState(() {
+                  _text = null;
+                });
+              },
+            )
+          ],
+        ),
+        body: Column(
           children: <Widget>[
-            TextField(
-              controller: myController,
-            ),
-            Text(
-              _text != null ? '$_text' : 'Watson Response Here',
-              style: Theme.of(context).textTheme.display1,
+            // Expanded(
+            //   child: ListView(
+            //     children: <Widget>[
+            //       BoxMessage(),
+            //       BoxMessage(),
+            //       BoxMessage(),
+            //     ],
+            //   ),
+            // ),
+            // Divider(height: 1.0,),
+            // Container(
+            //   decoration: BoxDecoration(color: Colors.white),
+            //   child: TextComposer(),
+            // )
+
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  TextField(
+                    controller: myController,
+                  ),
+                  Text(
+                    _text != null ? '$_text' : 'Resposta do Watson',
+                    style: Theme.of(context).textTheme.display1,
+                  ),
+                ],
+              ),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _callWatsonAssistant,
-        child: Icon(Icons.send),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _callWatsonAssistant,
+          child: Icon(Icons.send),
+        ),
       ),
     );
   }
@@ -87,5 +110,86 @@ class _ChatScreenState extends State<ChatScreen> {
   void dispose() {
     myController.dispose();
     super.dispose();
+  }
+}
+
+class TextComposer extends StatefulWidget {
+  @override
+  _TextComposerState createState() => _TextComposerState();
+}
+
+class _TextComposerState extends State<TextComposer> {
+  bool _isComposing = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).accentColor),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: <Widget>[
+            Container(
+              child: IconButton(
+                icon: Icon(Icons.photo_camera),
+                onPressed: () {},
+              ),
+            ),
+            Expanded(
+              child: TextField(
+                decoration:
+                    InputDecoration.collapsed(hintText: "Digite a mensagem"),
+                onChanged: (text) {
+                  setState(() {
+                    _isComposing = text.length > 0;
+                  });
+                },
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 4.0),
+              child: IconButton(
+                icon: Icon(Icons.send),
+                onPressed: _isComposing ? (){} :null,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BoxMessage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(right: 16.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage("https://www.ibm.com/watson/assets/duo/img/common/avatar_purple.png"),
+            ),
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text("Stark",
+                style:Theme.of(context).textTheme.subhead,
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 5.0),
+                  child: Text("teste"),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
